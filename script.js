cbc10fe5bc16b9d82eddae02522da067286f7566
iivic17 <ilijaivic17@gmail.com>
2024-12-23 16:45:54 +0100

    Show initial map

--- script.js
+++ script.js
@@ -26,7 +26,21 @@ const inputElevation = document.querySelector(".form__input--elevation");
 navigator.geolocation.getCurrentPosition(
   function (data) {
     const { latitude, longitude } = data.coords;
-    console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+    const coords = [latitude, longitude]
+
+    const map = L.map("map").setView(coords, 14);
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      .openPopup();
   },
   function () {
     alert("Could not get your position!");
